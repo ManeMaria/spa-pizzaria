fbab0f117effd54f6caf0cc34dee16bc1cb315ed
ManeMaria <mirandacesar598@gmail.com>
2021-01-16 10:10:12 -0300

    novas atualizações 1/3

--- src/components/Header/MenuMobile/index.js
+++ src/components/Header/MenuMobile/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Animation from './Animation';
 import { ContainerMenuMobile, ContentMenu, AnchorSections} from './styles'; //, AnchorSections
 function MenuMobile() {
@@ -6,30 +6,42 @@ function MenuMobile() {
     const [ animation, setAnimation ] = useState(false)
     const openAndCloseMenu = (ev) =>{
         //o classList.toggle já identifica se é para fechar, se estiver aberto
+        ev.stopPropagation();
         const openMenuAndClose = document.querySelector("#menu");
         const animationOnOff = openMenuAndClose.classList.toggle('open');
-        ev.stopPropagation();
         setAnimation(animationOnOff)
     }   
 
-   /**
-    * 
-    * 
-    *  <AnchorSections>
-            <p>PROMOÇÕES</p>
-        </AnchorSections>
-        <AnchorSections>
-            <p>LOCALIZAÇÃO</p>
-        </AnchorSections>
-        <AnchorSections>
-            <p>CADASTRAR</p>
-        </AnchorSections>
-       
-    */
+    const ref = useRef();
+    const OutSideClickTheBox = (r, callback) => {
+        /*através do useEffect se cria um evento de click e seu desmonte. Por meio do evento, chama
+        *   o handleClick que averigua se o click foi num component  filho, se não,     * *chama a função de retorno.
+        */
+        const handleClick = (e) =>{
+            if(r.current && !ref.current.contains(e.target)){
+                callback();
+            }
+        }
+
+        useEffect(()=>{
+            document.addEventListener('click', handleClick );
+            
+            return ()=> {
+                document.removeEventListener('click', handleClick );
+            };
+        });
+    };
+
+    OutSideClickTheBox(ref, ()=>{
+        //imita a mesma função do openAndCloseMenu, só que apenas fecha.
+        const openMenuAndClose = document.querySelector("#menu");
+        const animationOnOff = openMenuAndClose.classList.remove('open');
+        setAnimation(animationOnOff);
+    })
   return (
    <ContainerMenuMobile>
-      
-    <ContentMenu id = 'menu' > 
+      s
+    <ContentMenu id = 'menu' ref={ref}> 
         <button type='button' onClick={(e)  => openAndCloseMenu(e)}>
                 <Animation checked={animation} />
             </button>
